262e5b6b3cb42986c80a57a1ef38562d4a288681
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-15 15:52:11 +0300

    Export twin dashboard service contract

--- src/robot_systems/twin_robot/applications/dashboard/service/__init__.py
+++ src/robot_systems/twin_robot/applications/dashboard/service/__init__.py
@@ -1,3 +1,8 @@
-from src.robot_systems.twin_robot.applications.dashboard.service.twin_dashboard_service import TwinDashboardService
+from src.robot_systems.twin_robot.applications.dashboard.service.i_twin_dashboard_service import (
+    ITwinDashboardService,
+)
+from src.robot_systems.twin_robot.applications.dashboard.service.twin_dashboard_service import (
+    TwinDashboardService,
+)
 
-__all__ = ["TwinDashboardService"]
+__all__ = ["ITwinDashboardService", "TwinDashboardService"]
