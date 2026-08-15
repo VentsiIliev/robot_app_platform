c7a063c688c9ebb37d23bc6255b892b9cf1b4712
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-15 15:51:17 +0300

    Export choreography setup controller

---
+++ src/robot_systems/twin_robot/applications/choreography_setup/controller/__init__.py
@@ -0,0 +1,5 @@
+from src.robot_systems.twin_robot.applications.choreography_setup.controller.choreography_setup_controller import (
+    ChoreographySetupController,
+)
+
+__all__ = ["ChoreographySetupController"]
