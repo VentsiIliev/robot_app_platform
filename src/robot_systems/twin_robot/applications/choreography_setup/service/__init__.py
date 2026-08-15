2cc834b2a9be20bbfcb6cd5bde25a85bed777b18
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-15 15:51:52 +0300

    Export choreography setup service contract

--- src/robot_systems/twin_robot/applications/choreography_setup/service/__init__.py
+++ src/robot_systems/twin_robot/applications/choreography_setup/service/__init__.py
@@ -1,3 +1,8 @@
-from src.robot_systems.twin_robot.applications.choreography_setup.service.choreography_setup_service import ChoreographySetupService
+from src.robot_systems.twin_robot.applications.choreography_setup.service.choreography_setup_service import (
+    ChoreographySetupService,
+)
+from src.robot_systems.twin_robot.applications.choreography_setup.service.i_choreography_setup_service import (
+    IChoreographySetupService,
+)
 
-__all__ = ["ChoreographySetupService"]
+__all__ = ["ChoreographySetupService", "IChoreographySetupService"]
